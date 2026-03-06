24920ea7874f937336bed0ab1378ce25d49056f8
Harshita Nagpal <harshita.nagpal005@gmail.com>
2026-03-06 13:27:47 +0530

    add ons

--- deepscan-frontend/src/components/Navbar.jsx
+++ deepscan-frontend/src/components/Navbar.jsx
@@ -1,40 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HowItWorksModal from './HowItWorksModal';
 
 export default function Navbar() {
+  const [showHowItWorks, setShowHowItWorks] = useState(false);
+
   const scrollToAnalyzer = () => {
     document.getElementById('analyzer')?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
-    <header className="navbar">
-      <div className="navbar__inner">
-        <div className="navbar__logo">
-          <span className="navbar__logo-text">DeepScan</span>
-        </div>
+    <>
+      <header className="navbar">
+        <div className="navbar__inner">
+          <div className="navbar__logo">
+            <span className="navbar__logo-text">DeepScan</span>
+          </div>
 
-        <nav className="navbar__links">
-          <a href="#analyzer" className="navbar__link">How it works</a>
-          <a href="#analyzer" className="navbar__link">Features</a>
-          <a href="#analyzer" className="navbar__link">About</a>
-        </nav>
+          <nav className="navbar__links">
+            <button
+              type="button"
+              className="navbar__link navbar__link--btn"
+              onClick={() => setShowHowItWorks(true)}
+            >
+              How it works
+            </button>
+            <a href="#analyzer" className="navbar__link">Features</a>
+            <a href="#analyzer" className="navbar__link">About</a>
+          </nav>
 
-        <div className="navbar__actions">
-          <button
-            type="button"
-            className="navbar__btn navbar__btn--secondary"
-            onClick={scrollToAnalyzer}
-          >
-            Learn more
-          </button>
-          <button
-            type="button"
-            className="navbar__btn navbar__btn--primary"
-            onClick={scrollToAnalyzer}
-          >
-            Try now
-          </button>
+          <div className="navbar__actions">
+            <button
+              type="button"
+              className="navbar__btn navbar__btn--secondary"
+              onClick={() => setShowHowItWorks(true)}
+            >
+              Learn more
+            </button>
+            <button
+              type="button"
+              className="navbar__btn navbar__btn--primary"
+              onClick={scrollToAnalyzer}
+            >
+              Try now
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <HowItWorksModal isOpen={showHowItWorks} onClose={() => setShowHowItWorks(false)} />
+    </>
   );
 }
